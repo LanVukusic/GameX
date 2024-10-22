abab21beb6e2dcf7cb096a64882d462e28e01684
Lan Vukušič <lan.vukusic@gmail.com>
2024-10-22 11:31:55 +0200

    main camera follow and zoom

--- controller_web/src/WebsocketLogic.ts
+++ controller_web/src/WebsocketLogic.ts
@@ -2,7 +2,7 @@ import useWebSocket from "react-use-websocket";
 import { MSG } from "./dtos";
 
 export const useGameSocket = () => {
-  const socket = useWebSocket("ws://192.168.151.1:9999");
+  const socket = useWebSocket("ws://192.168.12.1:9999");
 
   const sendMsg = (msg: MSG) => {
     socket.sendJsonMessage(msg);
