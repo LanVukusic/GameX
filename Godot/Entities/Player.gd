fcec4eae3e582c44c7704a074001ea9cd5556b11
Lan Vukušič <lan.vukusic@gmail.com>
2024-10-31 19:11:04 +0100

    only one player script now

--- Godot/Entities/Player.gd
+++ Godot/Entities/Player.gd
@@ -2,7 +2,7 @@ class_name Player
 extends CharacterBody2D
 
 @export_category("Movement config")
-@export var moveSpeed: float
+@export var moveSpeed: float = 400
 @export var multiplayerId: int = 0
 @export var color: Color
 
@@ -14,11 +14,9 @@ extends CharacterBody2D
 signal moveVec(vec: Vector2)
 signal lookVec(vec: Vector2)
 signal lamp()
-
+signal connect(color: Color, name: String)
 
 var _input_direction = Vector2(0, 0)
-
-
 
 func set_input_direction(vec: Vector2):
 	_input_direction = vec
@@ -33,11 +31,16 @@ func get_input():
 func toggle_lamp():
 	$PointLight2D.enabled = !$PointLight2D.enabled
 
+func conn(col: Color, _name: String):
+	color = col
+	$ModulatableSprite.modulate = col
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	moveVec.connect(set_input_direction)
 	lookVec.connect(set_look_direction)
 	lamp.connect(toggle_lamp)
+	connect.connect(conn)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
