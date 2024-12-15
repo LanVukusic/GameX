324bc4a951f1988f74f151dec697a6e0d8420bdf
Lan Vukušič <lan.vukusic@gmail.com>
2024-12-15 14:48:22 +0100

    multitouch sensor for DnD kit

--- Controller_web/src/Components/Joysticks/ThemedJoystick.tsx
+++ Controller_web/src/Components/Joysticks/ThemedJoystick.tsx
@@ -11,7 +11,7 @@ export const ThemedJoystick = ({ ...other }: IJoystickProps) => {
   const parsedColor = parseThemeColor({ color: theme.primaryColor, theme });
   return (
     <Joystick
-      baseColor={alpha(lighten(parsedColor.value, 0.5), 0.2)}
+      baseColor={alpha(lighten(parsedColor.value, 0.4), 0.2)}
       stickColor={alpha(lighten(parsedColor.value, 0.1), 0.8)}
       {...other}
     />
