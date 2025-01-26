37d730d97bf17e082d8a334abdc89c6658705178
Lan Vukušič <lan.vukusic@gmail.com>
2025-01-26 19:08:04 +0100

    basic daynight time

--- Godot/daytime_manager.gd
+++ Godot/daytime_manager.gd
@@ -19,7 +19,7 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _physics_process(delta: float) -> void:
+func _physics_process(_delta: float) -> void:
 	# set time accoring to timer
 	if (enable_time_tracking):
 		var day_length = dayTimer.wait_time
